f6d831d25bed20138189fad7d65fe6ebfd2b6b9d
PoltX <yasserbelal2005@gmail.com>
2026-03-06 02:25:58 +0200

    implement product creation feature with dynamic elements in main.js

--- main.js
+++ main.js
@@ -1,11 +1,41 @@
-let myElement = document.createElement("div");
-let myAttribute = document.createAttribute("data-custom");
-let myText = document.createTextNode("Product One");
-let myComment = document.createComment("This is a div");
-myElement.className = "product";
-myElement.setAttributeNode(myAttribute);
-myElement.setAttribute("data-test","test value");
-myElement.appendChild(myComment);
-myElement.appendChild(myText);
+let btn = document.getElementById("create");
 
-document.body.appendChild(myElement);
+let form = document.getElementById("productForm");
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault(); 
+  let numberOfProducts = Number(document.getElementById("number1").value);
+  console.log(numberOfProducts);
+  for (let index = 1; index <= numberOfProducts; index++) {
+    let myMainElement = document.createElement("div");
+    let myHeading = document.createElement("h2");
+    let myParagraph = document.createElement("p");
+
+    myMainElement.style.color = "red";
+    myMainElement.style.backgroundColor = "black";
+    myMainElement.style.fontSize = "20px";
+    myMainElement.style.padding = "10px";
+    myMainElement.style.margin = "10px";
+    myMainElement.style.border = "1px solid white";
+    myMainElement.style.borderRadius = "5px";
+    myMainElement.style.textAlign = "center";
+    myMainElement.style.width = "200px";
+    myMainElement.style.height = "200px";
+    myMainElement.style.display = "inline-block";
+
+    let myHeadingText = document.createTextNode("Product Title " + index);
+    let myParagraphText = document.createTextNode(
+      "Product Description " + index,
+    );
+
+    myHeading.appendChild(myHeadingText);
+    myMainElement.appendChild(myHeading);
+
+    myParagraph.appendChild(myParagraphText);
+    myMainElement.appendChild(myParagraph);
+
+    myMainElement.className = "product";
+
+    document.body.appendChild(myMainElement);
+  }
+});
